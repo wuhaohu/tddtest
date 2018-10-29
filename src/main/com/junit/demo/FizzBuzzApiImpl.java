652b38fd17899f070eeb1307ce142c993cbf0d78
wuhaohu <287202127@qq.com>
2018-10-29 22:27:21 +0800

    FizzBuzz问题业务代码TDD方法重构

--- src/main/com/junit/demo/FizzBuzzApiImpl.java
+++ src/main/com/junit/demo/FizzBuzzApiImpl.java
@@ -2,32 +2,19 @@ package com.junit.demo;
 
 public class FizzBuzzApiImpl implements FizzBuzzApi {
     public String getFizzBuzz(int i) {
-        String result = String.valueOf(i);
+        StringBuffer result = new StringBuffer();
         if (i % 3 == 0) {
-            result = "Fizz";
-            if (i % 5 == 0) {
-                result = "FizzBuzz";
-                if (i % 7 == 0) {
-                    result = "FizzBuzzSuzz";
-                }
-                return result;
-            }
-            if (i % 7 == 0) {
-                result = "FizzSuzz";
-            }
-            return result;
+            result.append("Fizz");
         }
         if (i % 5 == 0) {
-            result = "Buzz";
-            if (i % 7 == 0) {
-                result = "BuzzSuzz";
-            }
-            return result;
+            result.append("Buzz");
         }
         if (i % 7 == 0) {
-            result = "Suzz";
-            return result;
+            result.append("Suzz");
         }
-        return result;
+        if (result.length() == 0) {
+            result.append(i);
+        }
+        return result.toString();
     }
 }
