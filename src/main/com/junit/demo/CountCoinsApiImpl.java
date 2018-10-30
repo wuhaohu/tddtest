161664099fca9540202cf15fe5443e48ed9a4c32
wuhaohu <287202127@qq.com>
2018-10-30 13:23:53 +0800

    CountCoins问题暴力递归业务实现

--- src/main/com/junit/demo/CountCoinsApiImpl.java
+++ src/main/com/junit/demo/CountCoinsApiImpl.java
@@ -1,8 +1,25 @@
 package com.junit.demo;
 
 public class CountCoinsApiImpl implements CountCoinsApi {
-    @Override
     public int getCountWays(int aim) {
-        return 0;
+        int[] coins = {1, 5, 10, 25};
+        int changeWays = 0;
+        if (aim == 0) {
+            return changeWays;
+        } else
+            changeWays = changeCoinProcess(coins, 0, aim);
+        return changeWays;
+    }
+
+    public int changeCoinProcess(int[] coins, int index, int aim) {
+        int changeWays = 0;
+        if (index == coins.length) {
+            changeWays = aim == 0 ? 1 : 0;
+        } else {
+            for (int i = 0; coins[index] * i <= aim; i++) {
+                changeWays += changeCoinProcess(coins, index + 1, aim - coins[index] * i);
+            }
+        }
+        return changeWays;
     }
 }
